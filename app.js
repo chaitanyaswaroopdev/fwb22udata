58f4309badd49b5784eb890e793c789d2333fbe9
Chaitanya Swaroop <62442765+chaitanyaswaroopdev@users.noreply.github.com>
2021-10-31 21:50:37 -0500

    eight results

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var gasRouter = require("./routes/gas");
+//var gasRouter = require("./routes/gas");
 //var addmodsRouter = require('./routes/addmods');
 //var selectorRouter = require('./routes/selector');
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/gas", gasRouter);
+//app.use("/gas", gasRouter);
 //app.use('/addmods',addmodsRouter);
 //app.use('/selector',selectorRouter);
 
