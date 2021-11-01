c7b9e89ba2e0d96d733d593dea909d387278eb5b
Chaitanya Swaroop <62442765+chaitanyaswaroopdev@users.noreply.github.com>
2021-11-01 18:03:02 -0500

    eleven commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var restaurantRouter = require("./routes/restaurant");
-//var addmodsRouter = require('./routes/addmods');
+var addmodsRouter = require("./routes/addmods");
 //var selectorRouter = require('./routes/selector');
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/restaurant", restaurantRouter);
-//app.use('/addmods',addmodsRouter);
+app.use("/addmods", addmodsRouter);
 //app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
